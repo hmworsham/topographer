d4c58959e1ef0503b5e6744d8663790dbc07e8e2
hmworsham <hmworsham@gmail.com>
2023-01-30 09:57:24 -0700

    added notes and references

--- R/heatload.R
+++ R/heatload.R
@@ -7,6 +7,21 @@
 #' @param unit units that aspect are in, either degrees or radians
 #' @param fold angle along which aspect should be folded
 #' @return total heat load value 
+#' 
+#' @note 
+#' Heat load is a unitless index of heat load, rather than a direct estimate of 
+#' incident radiation. The approach implements Equation 3 from McCune and Keon (2002),
+#' using inputs of latitude. The equation holds between latitudes of ±30-60º and 
+#' slope angles <= 60º. Their 3rd equation is valid for the narrowest input ranges 
+#' but has the highest R^2 and is suitable for many settings. I may implement
+#' equations 1 and 2 in the future. NOTE: the estimates are somewhat rudimentary 
+#' and do not take into account cloud cover, regional differences in the atmospheric 
+#' coefficient, or shading by adjacent topography.
+#' 
+#' @references
+#' McCune, B. and D. Keon (2002) Equations for potential annual direct incident
+#'   radiation and heat load. Journal of Vegetation Science 13:603-606.
+#' 
 #' @export
 #'
 #' @examples
