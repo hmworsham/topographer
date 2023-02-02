bbad9edde1166e8bb07d91465a9e2d22081d858d
hmworsham <hmworsham@gmail.com>
2023-02-02 11:39:48 -0700

    created cos and sin aspect functions for export

---
+++ R/cos.aspect.R
@@ -0,0 +1,32 @@
+#' Cosine-transform aspect
+#' 
+#' @description Transforms simple cardinal aspect values into their cosines
+#' @param x A vector, dataframe, or raster of aspect values
+#' @param unit Character. Units that aspect are in natively, either degrees or radians
+#' @return A vector, dataframe, or raster of cosine-transformed aspect values
+#' @export
+#' @examples
+#' \dontrun{
+#' library(terra)
+#' crb <- readRDS(system.file(file.path('data', 'crb.rds'), package='topographer'))
+#' a <- terra::terrain(crb, v='aspect')
+#' u <- 'deg'
+#' c.aspect <- cos.aspect(a, u)
+#' plot(c.aspect)
+#' }
+
+# Cosine transform aspect
+cos.aspect <- function(x, unit=c('rad','deg')) {
+  if (unit %in% c('rad','deg')) {
+    deg = F
+    if (unit=='deg') {
+      x = d2r(x)
+      deg = T
+    }
+    cos.asp <- cos(x)
+    names(cos.asp) <- 'usgs_cosaspect_100m'
+    if (deg) {return(r2d(cos.asp))
+    } else {return(cos.asp)}
+  } else print('unit must be rad or deg')
+}
+
---
+++ R/sin.aspect.R
@@ -0,0 +1,30 @@
+#' Cosine-transform aspect
+#' 
+#' @description Transforms simple cardinal aspect values into their sines
+#' @param x A vector, dataframe, or raster of aspect values
+#' @param unit Character. Units that aspect are in natively, either degrees or radians
+#' @return A vector, dataframe, or raster of sin-transformed aspect values
+#' @export 
+#' @examples
+#' \dontrun{
+#' library(terra)
+#' crb <- readRDS(system.file(file.path('data', 'crb.rds'), package='topographer'))
+#' a <- terra::terrain(crb, v='aspect')
+#' u <- 'deg'
+#' s.aspect <- sin.aspect(a, u)
+#' plot(s.aspect)
+#' }
+
+# Sine transform aspect
+sin.aspect <- function(x, unit=c('rad', 'deg')) {
+  match.arg(unit)
+  deg=F
+  if(unit=='deg') {
+    x = d2r(x)
+    deg = T
+  }
+  sin.asp <- sin(x)
+  names(sin.asp) <- 'usgs_cosaspect_100m'
+  if (deg) {return(r2d(sin.asp))
+  } else {return(sin.asp)}
+}
