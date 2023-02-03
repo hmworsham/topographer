cbdc6fe6148016c2a31469cf0d1784379a7799e0
hmworsham <hmworsham@gmail.com>
2023-02-03 11:54:00 -0700

    refactored to handle raster ingest outside function

--- R/zonals.R
+++ R/zonals.R
@@ -19,41 +19,42 @@
 #' TODO
 #' }
 
-zonals <- function(input, ras.source, topo.inputs, type=c('coord', 'sf'), radius, shape='rectangle') {
-
-  toporasters = flatten(lapply(topo.inputs, get.rasters, ras.source))
+zonals <- function(zones, rasters, type=c('coord', 'sf'), radius, shape='rectangle') {
 
   # If it's a coordinate or list of coordinates...
   if(type == 'coord'){
-    xy = input[,c(2,3)]
+    xy = zones[,c(2,3)]
     xy = st_as_sf(xy, coords=c('Longitude', 'Latitude'), crs=4326)
-    sites = st_transform(xy, crs(toporasters[[1]]))
+    sites = st_transform(xy, crs(rasters[[1]]))
 
     # Create polygons with polys
-    ID = input$Site_ID
+    ID = zones[,1]
+    print(ID)
     sites = st_buffer(sites, dist=radius, endCapStyle = 'SQUARE', joinStyle = 'MITRE')
     sites = vect(sites)
-    site_names = input$Site_ID
+    site_names = ID
   }
 
   # If it's a shapefile...
   else if(type == 'sf'){
-    sites = vect(input)
-    sites = terra::project(sites, crs(toporasters[[1]]))
+    sites = vect(zones)
+    sites = terra::project(sites, crs(rasters[[1]]))
     site_names = sites$PLOT_ID
   }
 
   # Plot the coordinates on top of the DEM
-  #plot(toporasters[[2]])
+  #plot(rasters[[2]])
   #plot(sites, color='blue', size=20, add = T)
 
   # Extract values from specified factors
-  topovals = lapply(toporasters, terra::extract, sites, fun = mean)
+  topovals = lapply(rasters, terra::extract, sites, fun = mean)
 
   # Return the values
   topovals.df = do.call('cbind', (lapply(topovals, '[', 2)))
-  colnames(topovals.df) <- sapply(toporasters, names)
-  rownames(topovals.df) <- site_names
+  colnames(topovals.df) <- sapply(rasters, names)
+  topovals.df$Location_ID <- site_names
+  topovals.df <- topovals.df[c(length(topovals.df), 2:length(topovals.df)-1)]
 
   return(topovals.df)
 }
+
