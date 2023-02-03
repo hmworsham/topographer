bfe435b6e7afbdfdd4a6ddd8ea4e6900178df307
hmworsham <hmworsham@gmail.com>
2023-02-03 13:11:03 -0700

    refactored for relative paths

--- R/get.rasters.R
+++ R/get.rasters.R
@@ -1,11 +1,10 @@
-#' Source rasters from local data directory
+#' Ingest rasters from data directory
 #'
-#' @description
-#' Sources rasters from a local directory using terra::rast and stores in a list of SpatRaster objects.
+#' @description Sources rasters from a local directory using terra::rast and
+#'   stores in a list of SpatRaster objects.
 #' @importFrom terra rast
-#' @param x a string containing a search pattern for a subdirectory name indicating the 
-#' raster data of interest
-#' @param dir a string containing a directory path where rasters are stored
+#' @param x Character vector. Search pattern for the raster data of interest
+#' @param path Character vector. Directory path where rasters are stored
 #' @return a list of SpatRaster objects for serial processing
 #' @export
 #'
@@ -15,9 +14,10 @@
 #' ras.ls <- get.rasters('raster', rasdir)
 #' }
 
-get.rasters = function(x, dir){
-  xpath = file.path(dir, x)
-  xtif = list.files(xpath, pattern = 'tif$', full.names = T)
+get.rasters = function(x, path){
+  list.files(path,
+             paste0('*', x, '.tif', collapse='|'),
+             full.names=T)
   xras = lapply(xtif, rast)
   return(xras)
 }
